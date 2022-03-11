e94b4b33a1631419736e2ed28d87c3c175ce6b18
Sandaly Singh <56186048+sandy502@users.noreply.github.com>
2022-03-12 00:25:01 +0530

    Updated

--- MadLib.py
+++ MadLib.py
@@ -1,20 +1,15 @@
-#how to take input from users
-#a = int(input("Enter a number: "))
-#print(a)
-
-#2 inputs at a time
-#x, y = [int(x) for x in input("Enter two values: ").split(",")]
-#print("First number is {} and second number is {}".format(x, y))
-#print()
-
-#raw_input
+#Mad Libs is a phrasal template word game.
+#It consists of one player prompting others for a list of words to substitute for blanks in a story before reading aloud.
 
 print(": p MAD LIBS ( :")
 
 noun1 = input("Enter a Noun: ")
+
 print("World's Greatest "+noun1+ " Game")
 
 print("\nLet's start!!!")
+
 noun2 = input("Enter a Noun: ")
 adjective1 = input("Enter an Adjective: ")
+
 print("\nEat well, stay "+ adjective1 + " , die anyway.\nWe will now upgrade your " + noun2 + " , \nplease wait… \nSearching…  searching…\n :> still searching… \n\nSorry :p , NO "+ noun2 +" found…!")
