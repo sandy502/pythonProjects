018d3242c03338b5888cfcbcc681cc653b08594d
Sandaly Singh <56186048+sandy502@users.noreply.github.com>
2022-03-13 13:28:24 +0530

    commit

---
+++ DSA questions/question1.py
@@ -0,0 +1,46 @@
+#Question 1
+# nums = [437,315,322,431,686,264,442]
+# evendigits = 0
+# for i in range(len(nums)):
+#     count = 0
+#     while int(nums[i])>0 :
+#         count=count+1
+#         nums[i]=nums[i]/10
+            
+#     if count%2 == 0:
+#         evendigits = evendigits+1
+            
+# print(evendigits)
+
+
+#Question 2
+# accounts = [[2,8,7],[7,1,3],[1,9,5]]
+# max = 0
+
+# for i in range(len(accounts)):
+#     sum = 0
+#     for j in range(len(accounts[i])):
+#         sum = sum + accounts[i][j]
+
+#     if max < sum:
+#         max = sum
+                
+# print(max)
+
+#Question 3
+nums = [3,2,4] 
+target = 6
+lists = []
+numb = 0
+for i in range(len(nums)):
+    if nums[i] == target :
+        numb = nums[i]
+        print(i)
+    elif nums[i] < target :
+        numb = numb + nums[i]
+        lists.append(i)
+    else:
+        continue
+        #print(-1)  
+
+print(lists)        
