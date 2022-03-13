c110636de53cdc76e0e6993cc095d9c8cd054831
Sandaly Singh <56186048+sandy502@users.noreply.github.com>
2022-03-13 13:29:02 +0530

    commit

---
+++ Projects/MadLib.py
@@ -0,0 +1,20 @@
+#how to take input from users
+#a = int(input("Enter a number: "))
+#print(a)
+
+#2 inputs at a time
+#x, y = [int(x) for x in input("Enter two values: ").split(",")]
+#print("First number is {} and second number is {}".format(x, y))
+#print()
+
+#raw_input
+
+print(": p MAD LIBS ( :")
+
+noun1 = input("Enter a Noun: ")
+print("World's Greatest "+noun1+ " Game")
+
+print("\nLet's start!!!")
+noun2 = input("Enter a Noun: ")
+adjective1 = input("Enter an Adjective: ")
+print("\nEat well, stay "+ adjective1 + " , die anyway.\nWe will now upgrade your " + noun2 + " , \nplease wait… \nSearching…  searching…\n :> still searching… \n\nSorry :p , NO "+ noun2 +" found…!")
