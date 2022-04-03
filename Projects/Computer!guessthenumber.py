8d894e242b5e77dd1a285983fa189df67180b623
Sandaly Singh <56186048+sandy502@users.noreply.github.com>
2022-04-03 10:45:42 +0530

    updated

--- Projects/Computer!guessthenumber.py
+++ Projects/Computer!guessthenumber.py
@@ -11,16 +11,16 @@ def computer_guess(x):
             guess = random.randint(low, high)
         else:
             guess = low    
-        feedback = input(f'Is {guess} too high (h) or too low (l) or correct (c) ???').lower()
+        feedback = input(f'\nIs {guess} too high (h) or too low (l) or correct (c) ???').lower()
         if feedback == 'h':
             print('\nYe sab Doglapan hai. (You hipocrite !!!)...\n')
             high = guess - 1
         elif feedback == 'l':
-            print('\nBhai kya kar raha/rahi hai tu!!! Aisa kaise chalega!!! (Bro what are you doing!!! )\n\n')
+            print('\nBhai kya kar raha/rahi hai tu!!! Aisa kaise chalega!!! (Bro what are you doing!!! )\n')
             low = guess + 1
 
     print(f'\nComputer guessed the number {guess} correctly!!!' )
     print('\nBillkul bakwas tha ye!!!')            
 
-x = int(input("\nYOU FREAKY HUMAN!!! TELL ME UPTO WHICH HIGHER RANGE OF NUMBER SHOULD I GUESS ?\n"))
+x = int(input("\nYOU FREAKY HUMAN!!! TELL ME UPTO WHICH HIGHER RANGE OF NUMBER SHOULD I GUESS ?"))
 computer_guess(x)    
